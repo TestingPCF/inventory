fe67d008329263178036063aa1e0ea4efe29eb51
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-05-07 17:08:57 +0530

    Rabbit Config changes to inventory

---
+++ src/main/java/com/hcl/cloud/inventory/config/RabbitmqConfig.java
@@ -0,0 +1,73 @@
+package com.hcl.cloud.inventory.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConversionException;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+	public static final String EXCHANGE_NAME = "pocMqTest";
+	
+	public static final String ROUTING_KEY = "mqpox";
+	
+	public static final String QUEUE_SPECIFIC_NAME = "appSpecificQueue";
+
+	
+	@Bean
+	public TopicExchange mqExchange() {
+		return new TopicExchange(EXCHANGE_NAME);
+	}
+
+	@Bean
+	public Queue appQueueSpecific() {
+		return new Queue(QUEUE_SPECIFIC_NAME);
+	}
+
+	@Bean
+	public Binding declareBindingSpecific() {
+		return BindingBuilder.bind(appQueueSpecific()).to(mqExchange()).with(ROUTING_KEY);
+	}
+
+	
+//	@Bean
+//	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//		
+//		rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+//		
+//		return rabbitTemplate;
+//	}
+	 @Bean
+	    public SimpleRabbitListenerContainerFactory jsaFactory(ConnectionFactory connectionFactory,
+	            SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+	        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+	        configurer.configure(factory, connectionFactory);
+	        factory.setMessageConverter(producerJackson2MessageConverter());
+	        return factory;
+	    }
+
+	@Bean
+	public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+		Jackson2JsonMessageConverter converter=new Jackson2JsonMessageConverter();
+		return converter;
+	}
+	public static class ImplicitJsonMessageConverter extends Jackson2JsonMessageConverter {    
+        public ImplicitJsonMessageConverter( ) {
+            super();
+        }    
+        @Override
+        public Object fromMessage(Message message) throws MessageConversionException {
+            message.getMessageProperties().setContentType("application/json");
+            return super.fromMessage(message);
+        }
+    }
+}
