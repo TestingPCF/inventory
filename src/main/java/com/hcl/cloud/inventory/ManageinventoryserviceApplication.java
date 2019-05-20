2249ad563d5e82838e666b4b005f0c08ab205679
MohitKri <MohitKri@LP-5CD9027VP6.HCLT.CORP.HCL.IN>
2019-05-20 17:44:55 +0530

    updated for service registry

--- src/main/java/com/hcl/cloud/inventory/ManageinventoryserviceApplication.java
+++ src/main/java/com/hcl/cloud/inventory/ManageinventoryserviceApplication.java
@@ -2,18 +2,18 @@ package com.hcl.cloud.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import com.hcl.cloud.inventory.config.RabbitmqConfig;
 import com.hcl.cloud.inventory.config.SwaggerConfiguration;
 
-
 @SpringBootApplication
-@Import({SwaggerConfiguration.class,RabbitmqConfig.class})
+@Import({ SwaggerConfiguration.class, RabbitmqConfig.class })
+@EnableDiscoveryClient
 public class ManageinventoryserviceApplication {
-	public static void main(String[] args)
-	{
-		SpringApplication.run(ManageinventoryserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ManageinventoryserviceApplication.class, args);
+    }
 
 }
