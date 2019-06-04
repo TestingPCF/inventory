58da20341651f25fc4b265313ac4122bd14e1f2d
testingpcf <Pcftest@1947>
2019-06-04 16:48:01 +0530

    updates

--- src/test/java/com/hcl/cloud/inventory/tests/InventoryControllerTest.java
+++ src/test/java/com/hcl/cloud/inventory/tests/InventoryControllerTest.java
@@ -12,6 +12,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -32,7 +34,7 @@ import com.hcl.cloud.inventory.exception.ApiRuntimeException;
 import com.hcl.cloud.inventory.service.InventoryService;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest 
+@SpringBootTest
 @AutoConfigureMockMvc
 public class InventoryControllerTest {
 
@@ -182,26 +184,72 @@ public class InventoryControllerTest {
 	}
 
 	@Test
-	@DisplayName("Post /api/inventory -- Create Inventory")
-	public void testisProductQuantityAvailable() throws Exception {
-		InventoryItem mockItem = new InventoryItem("product1231", "I003", 10, true);
+	@DisplayName("Post /api/inventory -- isProductQuantityAvailable")
+	public void testisProductQuantityAvailableTrue() throws Exception {
+		InventoryItem mockItem = new InventoryItem("product1231hj", "I001", 15, true);
 		doReturn(Optional.of(mockItem)).when(service).getInventoryItem("I001");
 		mockMvc.perform(get("/I001/10")).andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
 				.andExpect(content().string("true"));
 
 	}
-	
-	@Test
-	@DisplayName("Delete /I001 - Delete Inventory")
-	public void testDeleteInventory() throws Exception {
-		InventoryItem mockItem = new InventoryItem("arqw42343", "I001", 12, true);
-		doReturn(Optional.of(mockItem)).when(service).getInventoryItem("I001");
 
-		mockMvc.perform(get("/{productCode}", "I001")).andExpect(status().isOk())
+	@Test
+	@DisplayName("Post /api/inventory -- isProductQuantityAvailable")
+	public void testisProductQuantityAvailableFalse() throws Exception {
+		InventoryItem mockItem = new InventoryItem("product1231hj", "I001", 10, true);
+		doReturn(Optional.of(mockItem)).when(service).getInventoryItem("I001");
+		mockMvc.perform(get("/I001/15")).andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-				.andExpect(jsonPath("$.skuCode", is("I001"))).andExpect(jsonPath("$.quantity", is(12)))
-				.andExpect(jsonPath("$.activeStatus", is(true))).andExpect(jsonPath("$.inStock", is(true)));
+				.andExpect(content().string("false"));
+
+	}
+
+	/*
+	 * @Test
+	 * 
+	 * @DisplayName("Delete /I001 - Delete Inventory") public void
+	 * testDeleteInventory() throws Exception { InventoryItem mockItem = new
+	 * InventoryItem("arqw42343", "I001", 12, true);
+	 * doReturn(Optional.of(mockItem)).when(service).getInventoryItem("I001");
+	 * 
+	 * mockMvc.perform(get("/{productCode}", "I001")).andExpect(status().isOk())
+	 * .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+	 * .andExpect(jsonPath("$.skuCode",
+	 * is("I001"))).andExpect(jsonPath("$.quantity", is(12)))
+	 * .andExpect(jsonPath("$.activeStatus",
+	 * is(true))).andExpect(jsonPath("$.inStock", is(true)));
+	 * 
+	 * }
+	 */
+
+	@Test
+
+	@DisplayName("Get  getAllInventory")
+	public void testgetAllInventory() throws Exception {
+		InventoryItem item1 = new InventoryItem();
+		InventoryItem item2 = new InventoryItem();
+		
+		item1.setSkuCode("I101");
+		item1.setActiveStatus(true);
+		item1.setQuantity(12);
+		
+		item2.setSkuCode("I102");
+		item2.setActiveStatus(true);
+		item2.setQuantity(14);
+		
+		
+		List<InventoryItem> mockItem = new ArrayList<InventoryItem>();
+		mockItem.add(item1);
+		mockItem.add(item2);
+		doReturn(mockItem).when(service).findAllInventory();
+
+		mockMvc.perform(get("")).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("$[0].skuCode", is("I101"))).andExpect(jsonPath("$[0].quantity", is(12)))
+				.andExpect(jsonPath("$[0].activeStatus", is(true))).andExpect(jsonPath("$[0].inStock", is(true))).
+				 andExpect(jsonPath("$[1].skuCode", is("I102"))).andExpect(jsonPath("$[1].quantity", is(14)))
+				.andExpect(jsonPath("$[1].activeStatus", is(true))).andExpect(jsonPath("$[1].inStock", is(true)));
 
 	}
 
