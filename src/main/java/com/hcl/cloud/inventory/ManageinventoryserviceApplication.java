a6562244a28fe01664e10afda3ab09e14c85fd2b
AmitMish <AmitMish@LP-5CD7490BQG.HCLT.CORP.HCL.IN>
2019-06-13 14:59:38 +0530

    Revert "Changes for CQRS Event Sourcing"
    This reverts commit a5b912453e5cad3db1dcce5e14c04b36e42ef298.

--- src/main/java/com/hcl/cloud/inventory/ManageinventoryserviceApplication.java
+++ src/main/java/com/hcl/cloud/inventory/ManageinventoryserviceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.hcl.cloud.inventory.config.RabbitmqConfig;
 import com.hcl.cloud.inventory.config.SwaggerConfiguration;
@@ -12,7 +11,6 @@ import com.hcl.cloud.inventory.config.SwaggerConfiguration;
 @SpringBootApplication
 @Import({ SwaggerConfiguration.class, RabbitmqConfig.class })
 @EnableDiscoveryClient
-@EnableJpaRepositories
 public class ManageinventoryserviceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hcl/cloud/inventory/dto/InventoryItem.java
+++ src/main/java/com/hcl/cloud/inventory/dto/InventoryItem.java
@@ -32,11 +32,5 @@ public class InventoryItem implements Serializable {
 	private String skuCode;
 	private long quantity;
 	private boolean activeStatus;
-	public InventoryItem(String skuCode, long quantity, boolean activeStatus) {
-		super();
-		this.skuCode = skuCode;
-		this.quantity = quantity;
-		this.activeStatus = activeStatus;
-	}
 
 }
